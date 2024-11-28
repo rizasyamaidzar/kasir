7ab96351573aebdfeaba939d5c9233f1baf863f3
rizasyamaidzar <rizasyamaidzar@gmail.com>
2024-11-28 20:37:49 +0700

    [Added] Navbar & content

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import NavbarComponent from "./component/NavbarComponent";
+import ListCategories from "./component/ListCategories";
+import Hasil from "./component/Hasil";
+import DaftarProduk from "./component/DaftarProduk";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavbarComponent />
+      <div class="grid grid-rows-1 grid-flow-col gap-4 mx-10 my-20">
+        <ListCategories />
+        <DaftarProduk />
+        <Hasil />
+      </div>
     </div>
   );
 }
---
+++ src/component/DaftarProduk.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+export default class DaftarProduk extends Component {
+  render() {
+    return (
+      <div className="mx-auto">
+        <a
+          href="#"
+          className="block max-w-full p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+        >
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Noteworthy technology acquisitions 2021
+          </h5>
+          <p className="font-normal text-gray-700 dark:text-gray-400">
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order.
+          </p>
+        </a>
+      </div>
+    );
+  }
+}
